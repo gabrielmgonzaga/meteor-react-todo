268f428d686347ba3e7cb57b2d206329b23728fa
Gabriel Gonzaga <gabrielmgonzaga@gmail.com>
2017-03-21 20:49:07 -0700

    Import sign-in form #13

--- imports/ui/AccountsUIWrapper.jsx
+++ imports/ui/AccountsUIWrapper.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import { Template } from 'meteor/templating'
 import { Blaze } from 'meteor/blaze'
--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { createContainer } from 'meteor/react-meteor-data'
 import { Tasks } from '../api/tasks.js'
 import Task from './Task.jsx'
+import AccountsUIWrapper from './AccountsUIWrapper.jsx'
 
 class App extends Component {
   constructor(props) {
@@ -61,6 +62,8 @@ class App extends Component {
             />
             Hide Completed Tasks
           </label>
+
+          <AccountsUIWrapper />
 
           <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
             <input
